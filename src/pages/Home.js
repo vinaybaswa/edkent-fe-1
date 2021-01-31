d2d35e93f52eeade7da40e962016f6160361aab5
vinay baswa <vinaybaswa@gmail.com>
2021-01-31 12:35:33 -0500

    final commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './Home.scss'
 import Cardlist from '../components/Cardlist'
-import { users } from './Userdata.js';
 
-function Home() {
-  return (
-    <div className='home'>
-      <button type="button" className="btn btn-primary">Upgrade</button>
-      <Cardlist users={users} />
-    </div>
-  );
+class Home extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users: users }))
+  }
+
+  render() {
+    return (
+      <div className='home'>
+        <button type="button" className="btn btn-primary">Upgrade</button>
+        <Cardlist users={this.state.users} />
+      </div>
+    );
+  }
+
 }
 
 export default Home;
