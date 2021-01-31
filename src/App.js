96a0276ae3a1eaa608ef5ce1b9b918d5d2f46ab6
vinay baswa <vinaybaswa@gmail.com>
2021-01-30 22:36:33 -0500

    install router-dom and build sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.scss';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar />
+        <Router>
+          <Navbar />
+          <Sidebar />
+        </Router>
       </header>
     </div>
   );
---
+++ src/components/Sidebar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData.js';
+import './Sidebar.scss';
+
+
+class Sidebar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonSelected: null
+    }
+  }
+
+  handleClick = buttonSelected => () => {
+    this.setState({ buttonSelected })
+  }
+
+  render() {
+    return (
+      <nav className='nav-menu'>
+        <ul className='nav-menu-items'>
+          {SidebarData.map((item, index) => {
+            return (
+              <li key={index}
+                className={index === this.state.buttonSelected ? 'nav-text-selected' : item.cName}
+                onClick={this.handleClick(index)}
+              >
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Sidebar;
